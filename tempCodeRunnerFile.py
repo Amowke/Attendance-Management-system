acdfd763ccbd242606cdb1f040acac130fdc2fb6
Emmanuel Ekow Quansah <127907206+Amowke@users.noreply.github.com>
2024-08-25 16:49:07 +0000

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-search_lbl = CTkLabel(right_frame, text="Search System", font=("Arial", 15, "bold"), text_color="black")
-# search_lbl.grid(row=0, column=0, padx=10, pady=5, sticky=W)
-
-# search_frame = CTkFrame(right_frame, fg_color="white", border_width=2, border_color="black", width=750, height=100)
-# search_frame.grid(row=1, column=0, padx=10, sticky=W)
